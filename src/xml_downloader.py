6153735367db8e1f91035fff106cc38df4a63727
Milad <115378370+miladsdev@users.noreply.github.com>
2023-02-06 22:57:48 +0100

    Update xml_downloader.py

--- src/xml_downloader.py
+++ src/xml_downloader.py
@@ -18,9 +18,11 @@ class xml_downloader:
         url = "https://www.losungen.de/fileadmin/media-losungen/download/Losung_%s_XML.zip" % self.year
         response = requests.get(url)
 
+        if (os.path.exists(self.directory + "data/")):
+            os.makedirs(path)
+
         with open(self.directory + "data/Losung_%s_XML.zip" % self.year, 'wb') as file:
             file.write(response.content)
 
         with zipfile.ZipFile(self.directory + "data/Losung_%s_XML.zip" % self.year, 'r') as zip_ref:
             zip_ref.extractall(self.directory + "data")
-
